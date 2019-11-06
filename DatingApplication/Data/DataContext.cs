89d3a7f6b56ba0523238440d089cb91a609f9ac5
Balaji Thamotharan <56838534+balajithamotharan-aenetworks@users.noreply.github.com>
2019-11-06 12:13:00 +0530

    Initial Commit

---
+++ DatingApplication/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DatingApplication/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace DatingApplication.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
